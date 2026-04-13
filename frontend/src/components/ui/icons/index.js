fb55903f2c0e04746c90fbe0ea6533e41b4da811
franqong <garciafranconicolas@hotmail.com>
2026-04-13 14:33:28 -0300

    feat: circle assets

--- frontend/src/components/ui/icons/index.js
+++ frontend/src/components/ui/icons/index.js
@@ -1,4 +1,7 @@
 export { default as PentaLogo } from './PentaLogo';
 export { default as BandIcon } from './BandIcon';
+export { default as BandIconCircle } from './BandIconCircle';
 export { default as SettingsIcon } from './SettingsIcon';
+export { default as SettingsIconCircle } from './SettingsIconCircle';
 export { default as LogoutIcon } from './LogoutIcon.jsx';
+export { default as LogoutIconCircle } from './LogoutIconCircle';
