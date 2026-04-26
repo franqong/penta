2d44c7819100a2e5a90c7326b233e812fd8d353b
franqong <garciafranconicolas@hotmail.com>
2026-04-25 22:17:56 -0300

    feat: songs page layout added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/layout/Footer/Footer.jsx";
 import Home from "./pages/Home.jsx";
 import Profile from "./pages/Profile.jsx";
 import Settings from "./pages/Settings.jsx";
+import SongsPage from "./pages/Songs.jsx";
 import MusicPlayer from "./components/features/MusicPlayer/MusicPlayer.jsx";
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
             <Route path="/" element={<Home onPlayTrack={setTrack} />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/settings" element={<Settings />} />
+            <Route path="/songs" element={<SongsPage />} />
           </Routes>
         </main>
         <MusicPlayer track={track} setTrack={setTrack} />
---
+++ frontend/src/pages/Songs.jsx
@@ -0,0 +1,14 @@
+import "./Songs.css";
+import { Link } from "react-router-dom";
+
+function SongsPage() {
+  return (
+    <main className="layout">
+      <div className="section">
+        <h1>Popular songs of the week</h1>
+      </div>
+    </main>
+  );
+}
+
+export default SongsPage;
